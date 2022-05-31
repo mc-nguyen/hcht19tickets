afe3220362ea32949d830d051268b80410daf5b2
Vy Nguyen <marvin.calvin.1705@gmail.com>
2022-05-31 00:15:04 -0700

    import churches data

--- src/pages/ChurchesConfig.js
+++ src/pages/ChurchesConfig.js
@@ -6,14 +6,15 @@ import {
     Button,
     IconButton,
     ListItem,
-    ListItemAvatar,
+    ListItemAvatar, ListItemSecondaryAction,
     ListItemText,
     Snackbar,
-    TextField
+    TextField,
+    List
 } from "@mui/material";
 import {Church, Delete, Send} from "@mui/icons-material";
 import {deepPurple} from "@mui/material/colors";
-import {doc, setDoc} from "firebase/firestore";
+import {doc, setDoc, getDocs, collection} from "firebase/firestore";
 import db from "../Firebase";
 
 class ChurchesConfig extends React.Component {
@@ -25,6 +26,24 @@ class ChurchesConfig extends React.Component {
             success: true,
             notification: false
         }
+    }
+
+    componentDidMount() {
+        this.importDataFromFirebase().then(r => console.log('successfully read'));
+    }
+
+    importDataFromFirebase = async () => {
+        const querySnapshot = await getDocs(collection(db, "churches"));
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            const church = {
+                id: doc.id,
+                name: doc.data().fullName
+            };
+            this.setState({
+                churches: [...this.state.churches, church]
+            });
+        });
     }
 
     exportData2Firebase = () => {
@@ -48,30 +67,39 @@ class ChurchesConfig extends React.Component {
         })
     }
 
+    removeItemFromFirebase = (id) => {
+        console.log(id + " worked!");
+    }
+
     render() {
         return (
             <Box sx={{
-                my: 3, pt: 1, borderRadius: 3,
+                my: 3, borderRadius: 3,
                 bgcolor: deepPurple[300],
-                width: 1/4
+                width: 1/2
             }}>
-                <ListItem
-                    secondaryAction={
-                        <IconButton edge="end" aria-label="delete">
-                            <Delete />
-                        </IconButton>
-                    }
-                >
-                    <ListItemAvatar>
-                        <Avatar sx={{ width: 56, height: 56, mr: 3 }}>
-                            <Church />
-                        </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText
-                        primary="Single-line item"
-                        sx={{fontSize: 30, fontWeight: 'bold'}}
-                    />
-                </ListItem>
+                <List dense={true}>
+                    { this.state.churches.map((church, index) => (
+                        <ListItem key={index}
+                                  secondaryAction={
+                                      <IconButton edge="end" aria-label="delete"
+                                                  onClick={this.removeItemFromFirebase(church.id)}
+                                      >
+                                          <Delete/>
+                                      </IconButton>
+                                  }
+                        >
+                            <ListItemAvatar>
+                                <Avatar sx={{width: 56, height: 56, mr: 3}}>
+                                    <Church/>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText>
+                                <h2>{church.name}</h2>
+                            </ListItemText>
+                        </ListItem>
+                    ))}
+                </List>
                 <Box sx={{ display: 'flex', alignItems: 'flex-end', m: 1, p:1, bgcolor: 'white', borderRadius: 3 }}>
                     <Church sx={{ color: 'action.active', mr: 1, my: 0.5, width: 30, height: 30 }} />
                     <TextField id="input-with-sx"
